a4fa6ba7d01bba9cacf3283526c254557ca58e46
Larissa Pychlau <larissa.pychlau@haw-hamburg.de>
2024-07-15 16:15:58 +0200

    Algorithm complete - TODO: Documentation

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,38 @@ import GraphMaker.LGraph;
 import Visualisation.GraphVisualizer;
 import org.graphstream.graph.Graph;
 
-
+/**
+ * Client to run the algorithm.
+ * NODENUMMBER: Number of nodes the graph has and that must be included in the route
+ * MAXEDGECOST: Maximum cost an edge can have - relevant only to have different costs(distances)
+ * POPULATIONSIZE: Number of individuals traversing the path. Same for each generation
+ * GENERATIONCOUNT: Number of generations. Each generation (>1) consists of the last ones best individuals and a mutation thereof.
+ */
 public class Main {
     public static void main(String[] args) {
 
         int NODENUMBER = 10;
         int MAXEDGECOST = 10;
+        double SELECTIONRATE = 0.7;
+        int POPULATIONSIZE = 20;
+        int GENERATIONCOUNT = 20;
 
+        // Initialize Graph
         LGraph lGraph = new LGraph(NODENUMBER, MAXEDGECOST);
 
-        Algorithm algorithm = new Algorithm(0.7, lGraph, 20, 20);
+        // Initialize Algorithm
+        Algorithm algorithm = new Algorithm(SELECTIONRATE, lGraph, POPULATIONSIZE, GENERATIONCOUNT);
+
+        // Visualize the graph, tried routes and shortest route
         visualizeGraph(lGraph, algorithm.getWinner());
 
     }
 
+    /**
+     * Method to hand the graph and result over to the GraphStream API using the GraphVisualizer class
+     * @param lGraph the generated graph that was traversed
+     * @param ant the individual resulting in the lowest cost/distance (Result of the algorithm)
+     */
     private static void visualizeGraph(LGraph lGraph, Ant ant) {
         Graph graph = GraphVisualizer.visualize(lGraph, ant);
         GraphVisualizer.displayGraph(graph);
